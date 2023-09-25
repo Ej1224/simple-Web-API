731f246f5fe4df893a38393d198461760d63a342
EJ Ladrica <39943828+Ej1224@users.noreply.github.com>
2023-09-25 18:41:10 +0800

    Update TransferMoneyAPITests.cs
    added vice versa transaction test case

--- IntegrationTesting/TransferMoneyAPITests.cs
+++ IntegrationTesting/TransferMoneyAPITests.cs
@@ -8,12 +8,12 @@ namespace IntegrationTesting
     public class TransferMoneyAPITests
     {
         [TestMethod]
-        public async Task TestConcurrency()
+        public async Task sameTransaction_testConcurrency()
         {
             var client = ProgramTest.NewClient;
-            var firstTransfer = TransferMoneyAsync(client);
+            var firstTransfer = TransferMoneyAsync(client, "202309211556581", "202309211556582", 500.00M);
 
-            var secondTransfer = TransferMoneyAsync(client);
+            var secondTransfer = TransferMoneyAsync(client, "202309211556581", "202309211556582", 500.00M);
 
             var alltask = await Task.WhenAll(firstTransfer, secondTransfer);
 
@@ -34,9 +34,36 @@ namespace IntegrationTesting
             Assert.AreEqual("Concurrency Exception", err_thrown);
         }
 
-        private async Task<HttpResponseMessage> TransferMoneyAsync(HttpClient client)
+        [TestMethod]
+        public async Task viceVersaTransactions_testConcurrency()
         {
-            return await client.PostAsync("api/TransferMoneyAPI?sourceAcctId=202309211556581&destAcctId=202309211556582&amount=500", null);
+            var client = ProgramTest.NewClient;
+            var transfer_A = TransferMoneyAsync(client, "202309211556581", "202309211556582", 500.00M);
+            var transfer_B = TransferMoneyAsync(client, "202309211556582", "202309211556581", 500.00M);
+
+            var alltask = await Task.WhenAll(transfer_A, transfer_B);
+
+            var transactionA_output = transfer_A.Result.Content.ReadFromJsonAsync<BankTransactionVM>();
+            var transactionB_output = transfer_B.Result.Content.ReadFromJsonAsync<BankTransactionVM>();
+
+            var err_thrown = "";
+
+            if (transactionA_output?.Result?.ErrorThrown != "")
+            {
+                err_thrown = transactionA_output?.Result?.ErrorThrown;
+            }
+            else if (transactionB_output?.Result?.ErrorThrown != "")
+            {
+                err_thrown = transactionB_output?.Result?.ErrorThrown;
+            }
+
+            Assert.AreEqual("Concurrency Exception", err_thrown);
+        }
+
+        private async Task<HttpResponseMessage> TransferMoneyAsync(HttpClient client, string source, string dest, decimal amount)
+        {
+            //return await client.PostAsync("api/TransferMoneyAPI?sourceAcctId=202309211556581&destAcctId=202309211556582&amount=500", null);
+            return await client.PostAsync("api/TransferMoneyAPI?sourceAcctId=" + source + "&destAcctId=" + dest + "&amount=" + amount, null);
         }
     }
 }
